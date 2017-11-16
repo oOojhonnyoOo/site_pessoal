f32b175527aa3f0a7d891729dcaf82e8bf69d787
João Neto <eujoaonetoti@outlook.com>
2017-11-16 15:34:13 -0300

    teste 03.02

--- testeEnvioEmail/enviarEmail.php
+++ testeEnvioEmail/enviarEmail.php
@@ -177,8 +177,15 @@ if($_GET['sendMail'] == "SendMailInCompleteForInformata"){
 // $return->statusMail = $response->statusCode();
 // $return->bodyMail   = $response->body();
 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'To: Informata joao.neto@informata.com.br' . "\r\n";
+$headers .= 'From: Informata <joao.neto@informata.com.br>' . "\r\n";
+$headers .= 'Cc: joao.neto@informata.com.br' . "\r\n";
+$headers .= 'Bcc: joao.neto@informata.com.br' . "\r\n";
+
 $return->to = $to;
-$return->StatusMail = mail($to, $subject, $message);
+$return->StatusMail = mail($to, $subject, $message, $headers);
 
 echo json_encode($return);
 
