f3b0a4d7e6ed4861892ecb4cdac026585e08863c
João Neto <eujoaonetoti@outlook.com>
2017-11-21 10:22:10 -0300

    finely

--- testeEnvioEmail/enviarEmail.php
+++ testeEnvioEmail/enviarEmail.php
@@ -96,7 +96,29 @@ if($_GET['sendMail'] == "SendMailCompleteForClient")
     </html>
     ";
 
-    include 'registraDados';
+    $sql = "delete from aluno where email = '".$_POST['email']."' order by id desc limit 1 ";
+    $return->sqlDelete   = $sql;
+    $return->queryDelete = mysql_query($sql);
+
+    $sql    = "INSERT INTO aluno(condicao, data, nome, email, telefone, segmento, tam_cd, itens_sku, qtd_postos_trabalho, notas_entrada_dia, n_pedidos_dia, qtd_caminhoes_dia, picking, notas_dia) VALUES (
+               '".$return->condicao."' , 
+               '".date('Y-m-d')."',
+               '".$_POST['nome']."' , 
+               '".$_POST['email']."' , 
+               '".$_POST['telefone']."' , 
+               '".$_POST['segmento']."' , 
+               '".$_POST['tam_cd']."' , 
+               '".$_POST['itens_sku']."' , 
+               '".$_POST['qtd_postos_trabalho']."' , 
+               '".$_POST['notas_entrada_dia']."' , 
+               '".$_POST['n_pedidos_dia']."' , 
+               '".$_POST['qtd_caminhoes_dia']."' , 
+               '".$_POST['picking']."' , 
+               '".$_POST['notas_dia']."' 
+               )
+              ";
+    $return->sqlInsert   = $sql;
+    $return->queryInsert = mysql_query($sql);
 
 }
 
