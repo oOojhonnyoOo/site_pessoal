e562b883164ee5c96c86bee89a5ae57ef957d92b
João Neto <eujoaonetoti@outlook.com>
2017-11-21 09:53:42 -0300

    teste cond 01

--- testeEnvioEmail/enviarEmail.php
+++ testeEnvioEmail/enviarEmail.php
@@ -1,7 +1,5 @@
 <?php 
 
-include "Conexao.php";
-
 $return = new stdClass();
 
 $emailInformata = "joao.neto@informata.com.br";
@@ -95,27 +93,6 @@ if($_GET['sendMail'] == "SendMailCompleteForClient")
     </body>
     </html>
     ";
-
-    // SALVANDO DADOS NO BANCO DE DADOS
-    $sql    = "INSERT INTO aluno(condicao, data, nome, email, telefone, segmento, tam_cd, itens_sku, qtd_postos_trabalho, notas_entrada_dia, n_pedidos_dia, qtd_caminhoes_dia, picking, notas_dia) VALUES (
-               '".$return->condicao."' , 
-               '".date('Y-m-d')."',
-               '".$_POST['nome']."' , 
-               '".$_POST['email']."' , 
-               '".$_POST['telefone']."' , 
-               '".$_POST['segmento']."' , 
-               '".$_POST['tam_cd']."' , 
-               '".$_POST['itens_sku']."' , 
-               '".$_POST['qtd_postos_trabalho']."' , 
-               '".$_POST['notas_entrada_dia']."' , 
-               '".$_POST['n_pedidos_dia']."' , 
-               '".$_POST['qtd_caminhoes_dia']."' , 
-               '".$_POST['picking']."' , 
-               '".$_POST['notas_dia']."' 
-               )
-              ";
-    $return->sql   = $sql;
-    $return->query = mysql_query($sql);
 
 }
 
