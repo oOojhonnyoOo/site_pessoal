f2081f66d88e10aaf4a05de457a9979a74d8c646
João Neto <eujoaonetoti@outlook.com>
2017-11-28 10:28:44 -0300

    registrando nova condicao para cadastros incompletos

--- testeEnvioEmail/registraDados.php
+++ testeEnvioEmail/registraDados.php
@@ -6,10 +6,8 @@ $sql = "delete from aluno where email = '".$_POST['email']."' and validado <> 1 
 $return['sqlDelete']   = $sql;
 $return['queryDelete'] = mysql_query($sql);
 
-$cond = isset($return->condicao)? $return->condicao : '1';
-
 $sql    = "INSERT INTO aluno(condicao, data, nome, email, telefone, segmento, tam_cd, itens_sku, qtd_postos_trabalho, notas_entrada_dia, n_pedidos_dia, qtd_caminhoes_dia, picking, notas_dia, validado) VALUES (
-           '".$cond."' , 
+           '-' , 
            '".date('Y-m-d')."',
            '".$_POST['nome']."' , 
            '".$_POST['email']."' , 
