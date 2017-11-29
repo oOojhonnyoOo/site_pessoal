36253fe7bad8e2e1416ed8db94f4f869144c7b53
João Neto <eujoaonetoti@outlook.com>
2017-11-29 10:33:41 -0300

    desabilitando opcao de sobrescrita

--- testeEnvioEmail/enviarEmail.php
+++ testeEnvioEmail/enviarEmail.php
@@ -96,9 +96,9 @@ if($_GET['sendMail'] == "SendMailCompleteForClient")
     </html>
     ";
 
-    $sql = "delete from aluno where email = '".$_POST['email']."' and validado <> 1 order by id desc limit 1 ";
-    $return->sqlDelete   = $sql;
-    $return->queryDelete = mysql_query($sql);
+//    $sql = "delete from aluno where email = '".$_POST['email']."' and validado <> 1 order by id desc limit 1 ";
+//    $return->sqlDelete   = $sql;
+//    $return->queryDelete = mysql_query($sql);
 
     $sql    = "INSERT INTO aluno(condicao, data, nome, email, telefone, segmento, tam_cd, itens_sku, qtd_postos_trabalho, notas_entrada_dia, n_pedidos_dia, qtd_caminhoes_dia, picking, notas_dia, validado) VALUES (
                '".$return->condicao."' , 
