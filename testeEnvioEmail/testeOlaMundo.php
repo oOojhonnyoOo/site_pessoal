9bdea2f3055d84b51ecdea457a0058fc836d12b4
João Neto <eujoaonetoti@outlook.com>
2017-12-12 09:07:04 -0300

    testando com php mailer

---
+++ testeEnvioEmail/testeOlaMundo.php
@@ -0,0 +1,43 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 
+    $mail->isSMTP();                                      
+    $mail->Host = 'aspmx.l.google.com';  
+    $mail->SMTPAuth = true;                               
+    $mail->Username = 'eujoaonetoprogramador@gmail.com';                 
+    $mail->Password = '123asd123asd';                           
+    $mail->SMTPSecure = 'tls';                            
+    $mail->Port = 25;                                    
+
+    //Recipients
+    $mail->setFrom('eujoaonetoprogramador@gmail.com', 'Mailer');
+    $mail->addAddress('eujoaonetoti@outlook.com', 'joao neto');     
+    $mail->addReplyTo('eujoaonetoprogramador@gmail.com', 'Information');
+    $mail->addCC('eujoaonetoprogramador@gmail.com');
+    $mail->addBCC('eujoaonetoprogramador@gmail.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    
+
+    //Content
+    $mail->isHTML(true);                                  
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
