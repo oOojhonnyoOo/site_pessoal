6adecd40d97200edf38a3a352a0011bc47d0f781
João Neto <eujoaonetoti@outlook.com>
2017-12-12 10:29:47 -0300

    testando envio de email com phpmailer tst 4

---
+++ testeEnvioEmail/testeOlaMundo4.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'PHPMailer-5.2.26/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = 2;
+$mail->Debugoutput = 'html';
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = 'tls';
+$mail->SMTPAuth = true;
+$mail->Username = "joao.neto@informata.com.br";
+$mail->Password = "123asd123asd";
+
+$mail->setFrom('eujoaonetoprogramador@gmail.com', 'Joao Neto');
+$mail->addReplyTo('eujoaonetoprogramador@gmail.com', 'joao neto');
+$mail->addAddress('eujoaonetoti@outlook.com', 'teste 123');
+
+$mail->Subject = 'PHPMailer GMail SMTP test';
+
+
+$mail->msgHTML("<html>ola mundo</html>");
+
+//Replace the plain text body with one created manually
+//$mail->AltBody = 'This is a plain-text message body';
+
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+
+}
